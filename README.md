eddc065c47944bd4a9b5994090d694329a1816e9
Jason Clemons <hello@jasonclemons.me>
2015-09-18 03:59:22 -0400

    Initial commit
    Signed-off-by: Jason Clemons <hello@jasonclemons.me>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# flarum-classifieds
+# Classifieds
 Classifieds extension for Flarum
+
+## How to install
+[Download the latest release](https://github.com/zilladotexe/flarum-classifieds/releases/latest). Unzip the archive into the `extensions` directory of your Flarum installation. Then go to the admin panel and enable the extension.
---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+* @package   zilladotexe/flarum-classifieds
+* @copyright Copyright (c) 2015 Jason Clemons <hello@jasonclemons.me>
+* @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+*/
+namespace zilladotexe\Flarum\Classifieds;
+
+use Flarum\Support\Extension as BaseExtension;
+use Illuminate\Events\Dispatcher;
